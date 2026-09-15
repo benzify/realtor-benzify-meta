8f0c6c366f11cb41bd2aea9cb9c33b0424ce9caa
rak100 <sbenztech@gmail.com>
2026-09-14 21:50:33 -0400

    added scheduling link

--- dist/config.js
+++ dist/config.js
@@ -1,2 +1,6 @@
 // Add an HTTPS scheduling URL and a direct MP4/WebM testimonial URL when ready.
-window.BENZIFY_CONFIG = Object.freeze({ bookingUrl: '', videoSrc: '', videoPoster: '' });
+window.BENZIFY_CONFIG = Object.freeze({
+  bookingUrl: 'https://calendly.com/rak-benzify/meta-ads-setup',
+  videoSrc: '',
+  videoPoster: ''
+});
